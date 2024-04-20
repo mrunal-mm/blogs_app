0dc0872bb44963878d3a5f656d5cb5082b1cef71
D3_Shubham_83967 <shubhambansode126@gmail.com>
2024-04-20 09:41:47 +0530

    created register api

--- server/test.js
+++ server/test.js
@@ -16,3 +16,25 @@ app.get('/', (req, res) => {
 app.listen(4000, '0.0.0.0', () => {
   console.log('Server started on http:/localhost:4000');
 });
+
+
+
+const express = require('express');
+const cryptojs = require('crypto-js');
+const db = require('../db');
+const router = express.Router();
+
+router.post('/register', (req, res) => {
+  const { full_name , email, password,phone_no ,created_time } = req.body;
+  const encryptedPassword = String(cryptojs.SHA256(password));
+  var data = { status: 'new user created', full_name , email, encryptedPassword, phone_no ,created_time };
+
+  const query = 'INSERT INTO usertb (full_name , email, password,phone_no ,created_time ) VALUES (?, ?, ?,?,?)';
+  db.pool.execute(query, [full_name , email, encryptedPassword, phone_no ,created_time ], (error, dbResults) => {
+    if (error) {
+      res.send('Internal Error');
+    } else {
+      res.send({ data, dbResults });
+    }
+  });
+});
