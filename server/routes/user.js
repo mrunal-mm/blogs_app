325144df2fe868393c6dfe7040df729caafa5552
mrunal-mm <mrunalmm2@gmail.com>
2024-04-20 15:25:20 +0530

    created view-all-blogs api

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,47 +1,70 @@
 const express = require("express");
-const db = require('../db');
-const global = require("../global")
+const db = require("../db");
+const global = require("../global");
 
 const router = express.Router();
 
-router.post("/add-blog", (req, res)=>{
-    const {title, contents, category_id} = req.body;
-    const user_id = global.user_id;
-    const query = "insert into blogs (title, contents, user_id, category_id) values (?, ?, ?, ?)";
+router.post("/add-blog", (req, res) => {
+  const { title, contents, category_id } = req.body;
+  const user_id = global.user_id;
+  const query =
+    "insert into blogs (title, contents, user_id, category_id) values (?, ?, ?, ?)";
 
-    db.pool.execute(query, [title, contents, user_id, category_id], (error, dbResults)=>{
-        if (error){
-            res.send("Internal error.");
+  db.pool.execute(
+    query,
+    [title, contents, user_id, category_id],
+    (error, dbResults) => {
+      if (error) {
+        res.send("Internal error.");
         return;
-        }
-        res.send({status: "new blog added.", dbResults});
-    })
-})
+      }
+      res.send({ status: "new blog added.", dbResults });
+    }
+  );
+});
 
-router.put("/edit-blog", (req, res)=>{
-    const {title, contents, category_id, blog_id} = req.body;
-    const query = "update blogs set title = ?, contents = ?, category_id = ? where blog_id = ?";
+router.put("/edit-blog", (req, res) => {
+  const { title, contents, category_id, blog_id } = req.body;
+  const query =
+    "update blogs set title = ?, contents = ?, category_id = ? where blog_id = ?";
 
-    db.pool.execute(query, [title, contents, category_id, blog_id], (error, dbResults)=>{
-        if (error){
-            res.send("Internal error.");
+  db.pool.execute(
+    query,
+    [title, contents, category_id, blog_id],
+    (error, dbResults) => {
+      if (error) {
+        res.send("Internal error.");
         return;
-        }
-        res.send({status: "blog edited", dbResults});
-    })
-})
+      }
+      res.send({ status: "blog edited", dbResults });
+    }
+  );
+});
 
-router.get("/view-my-blogs", (req, res)=>{
-    const user_id = global.user_id;
-    const query = "select * from blogs where user_id = ?";
+router.get("/view-my-blogs", (req, res) => {
+  const user_id = global.user_id;
+  const query = "select * from blogs where user_id = ?";
 
-    db.pool.execute(query, [user_id], (error, dbResults)=>{
-        if (error){
-            res.send("Internal error.");
-        return;
-        }
-        res.send(dbResults);
-    })
-})
+  db.pool.execute(query, [user_id], (error, dbResults) => {
+    if (error) {
+      res.send("Internal error.");
+      return;
+    }
+    res.send(dbResults);
+  });
+});
+
+router.get("/view-all-blogs", (req, res) => {
+  const user_id = global.user_id;
+  const query = "select * from blogs";
+
+  db.pool.execute(query, (error, dbResults) => {
+    if (error) {
+      res.send("Internal error.");
+      return;
+    }
+    res.send(dbResults);
+  });
+});
 
 module.exports = router;
